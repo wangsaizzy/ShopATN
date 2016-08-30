d2f35b19ba06b6d240088aa2bd7bee9a8a94a8a7
BearsG <1352864983@qq.com>
2016-08-30 10:20:43 +0800

    first commit

---
+++ ATNShop/ATNShop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ATNShop
+//
+//  Created by 王赛 on 16/8/30.
+//  Copyright © 2016年 王赛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
