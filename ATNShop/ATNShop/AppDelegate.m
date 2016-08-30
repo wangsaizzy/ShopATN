4a200764ea9e8e55be97e3fc386d8b55d867b4e7
dorayo <yomingyo@gmail.com>
2016-08-30 15:43:36 +0800

    添加了图标、修改了info.plist文件、Appdelegate.m

--- ATNShop/ATNShop/AppDelegate.m
+++ ATNShop/ATNShop/AppDelegate.m
@@ -7,7 +7,9 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ShopManagerController.h"
+#import "LoginAndRegistViewController.h"
+#import <AMapFoundationKit/AMapFoundationKit.h>
 @interface AppDelegate ()
 
 @end
@@ -16,8 +18,93 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    /**
+     *  设置启动页时间
+     */
+    [NSThread sleepForTimeInterval:1.0];
+    
+    
+    /**
+     *  创建窗口
+     */
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    /**
+     *  绑定根视图
+     */
+    [AMapServices sharedServices].apiKey = KAMap2DMapKey;
+    
+    //    self.mapView = [[MAMapView alloc] init];
+    //    _mapView.delegate = self;
+    //
+    //    NSString *longitude = [NSString stringWithFormat:@"%@", [UserModel defaultModel].longitude];
+    //    NSString *latitude = [NSString stringWithFormat:@"%@", [UserModel defaultModel].latitude];
+    //
+    //    if ([longitude floatValue] > 0 && [latitude floatValue] > 0) {
+    //
+    //        _mapView.showsUserLocation = NO;    //YES 为打开定位，NO为关闭定位
+    //
+    //
+    //
+    //    } else {
+    //
+    //        _mapView.showsUserLocation = YES;    //YES 为打开定位，NO为关闭定位
+    //        _mapView.userTrackingMode = MAUserTrackingModeFollowWithHeading;
+    //
+    //        [_mapView setZoomLevel:16.1 animated:YES];
+    //    }
+    
+    
+    [SVProgressHUD setMinimumDismissTimeInterval:1];
+    
+    [SVProgressHUD setDefaultMaskType:SVProgressHUDMaskTypeBlack];
+    
+    // 如果要检测网络状态的变化,必须用检测管理器的单例的startMonitoring
+    [[AFNetworkReachabilityManager sharedManager] startMonitoring];
+    
+    // 检测网络连接的单例,网络变化时的回调方法
+    [[AFNetworkReachabilityManager sharedManager] setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+        
+        switch (status) {
+            case AFNetworkReachabilityStatusUnknown:
+                [SVProgressHUD showSuccessWithStatus:@"当前未知网络"];
+                break;
+            case AFNetworkReachabilityStatusNotReachable:
+                [SVProgressHUD showSuccessWithStatus:@"无网络连接"];
+                break;
+            case AFNetworkReachabilityStatusReachableViaWWAN:
+                [SVProgressHUD showSuccessWithStatus:@"当前为3G/4G网络"];
+                break;
+            case AFNetworkReachabilityStatusReachableViaWiFi:
+                [SVProgressHUD showSuccessWithStatus:@"当前为WiFi网络"];
+                break;
+        }
+    }];
+    
+    
+    
+    ShopManagerController *shopVC = [[ShopManagerController alloc] init];
+    UINavigationController *navi = [[UINavigationController alloc] initWithRootViewController:shopVC];
+    
+    NSTimeInterval timeInterval = [self timeAgo];
+    
+    if (timeInterval < 604795) {
+        
+        self.window.rootViewController = navi;
+    } else {
+        
+        // 进入登录页面
+        self.window.rootViewController = navi;
+        [UserModel defaultModel].isAppLogin = NO;
+    }
     return YES;
+}
+
+- (NSTimeInterval)timeAgo{
+    
+    //1.创建时间与当前的时间差
+    NSTimeInterval intervel = [[NSDate date] timeIntervalSinceDate:[TimeTool unarchiveLoginDate]];
+    return intervel;
+    
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
